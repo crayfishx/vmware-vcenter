50788fac24ff218481ec5ba2c7ec0c956d885a17
Craig Dunn <craig@craigdunn.org>
2016-07-06 14:43:33 +0200

    use rspec mock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+$:.unshift File.dirname(__FILE__) + "/fixtures/modules/vmware_lib/lib"
+
+RSpec.configure do |config|
+  config.mock_framework = :rspec
+end
+
