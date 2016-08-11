3317b9786f8e0f8bae29d9bf9bd3abbdba37d0b1
Craig Dunn <craig@craigdunn.org>
2016-08-11 11:36:18 +0200

    rough poc

--- lib/puppet/type/esx_system_resource.rb
+++ lib/puppet/type/esx_system_resource.rb
@@ -2,60 +2,108 @@
 Puppet::Type.newtype(:esx_system_resource) do
   @doc = "This resource allows the configuration of system resources of a host that are viewed und er the 'System Resource Allocation' section of the vSphere client"
 
+  def self.title_patterns
+    identity = lambda { |x| x }
+    [
+      [
+        /^([^:]+):([^:]+)$/,
+        [ [:host, identity], [:name, identity] ]
+      ],
+      [
+        /^([^:]+)$/,
+        [[ :name, identity ]]
+      ]
+    ]
+  end
+
   newparam(:name) do
-    desc "A unique name that will allow for setting the same resource on multiple hosts or multiple resources on the same host through multiple resource calls within your puppet manifest."
+    desc "The name of the resource, eg cpu_limit"
+    isnamevar
   end
 
-  newparam(:host,) do
+  newparam(:host) do
     desc "ESX hostname or ip address."
+    isnamevar
   end
 
-  newparam(:system_resource) do
-    desc "The system resource to be managed"
-  end
+  newparam(:value) do
+    desc <<-EOT 
+    The value to assign to this resource setting, the value depends on the 
+    type of resource being managed, supported values are;
 
-  newproperty(:cpu_reservation) do
-    desc "System resource CPU reservation in MHz"
-    newvalues(/\d{1,}/)
-  end
+    cpu_reservation
+    ===============
+      The CPU reservsation specified in MHz
 
-  newproperty(:cpu_expandable_reservation) do
-    desc "Enable expandable reservation"
-    newvalues(:true, :false)
-  end
+    cpu_expandable_reservation
+    ==========================
+      Enable expandable reservation, valid values are true and false
 
-  newproperty(:cpu_limit) do
-    desc <<-EOT
-    Sets the CPU limit.  Acceptable values are a numeric value (in Mhz) or
-    the string "unlimited"
+    cpu_limit
+    =========
+      The CPU limit specified in MHz
+
+    cpu_unlimited
+    =============
+      Enable unlimited CPU resources, valid values are true and false
+
+    memory_reservation
+    ==================
+      Enable expandable memory reservation, valid values are true and false
+
+    memory_limit
+    ============
+      The memory limit specified in MB
+
+    memory_unlimited
+    ================
+      Enable unlimited memory resources, valuid values are true and false
     EOT
-    newvalues(/\d{1,}/, "unlimited")
-    munge do |value|
-      value = -1 if value == "unlimited"
-      value
+
+    case self[:name]
+
+    when "cpu_limit"
+      newvalues(/\d{1,}/, "unlimited")
+      munge do |value|
+        value = -1 if value == "unlimited"
+        value
+      end
+
+    when  "cpu_reservation"
+      newvalues(/\d{1,}/)
+
+    when "cpu_expandable_reservation"
+      newvalues(:true, :false)
+
+    when "cpu_limit"
+      newvalues(/\d{1,}/, "unlimited")
+      munge do |value|
+        value = -1 if value == "unlimited"
+        value
+      end
+
+    when "memory_reservation"
+      newvalues(/\d{1,}/)
+
+    when "memory_expandable_reservation"
+      newvalues(:true, :false)
+
+    when "memory_limit"
+      newvalues(/\d{1,}/, "unlimited")
+      munge do |value|
+        value = -1 if value == "unlimited"
+        value
+      end
+
+    when "memory_unlimited"
+      newvalues(:true,:false)
+      munge do |value|
+        if value == 'true'
+          @resource[:memory_limit] = -1
+        elsif value == 'false' && !(@resource[:memory_limit])
+          @resource[:memory_limit] = 0
+        end
+      end
     end
   end
-
-  newproperty(:memory_reservation) do
-    desc "System resource memory reservation in MB"
-    newvalues(/\d{1,}/)
-  end
- 
-  newproperty(:memory_expandable_reservation) do
-    desc "Enable expandable reservation"
-    newvalues(:true, :false)
-  end
- 
-  newproperty(:memory_limit) do
-    desc <<-EOT
-    Sets the memory limit.  Acceptable values are a numeric value (in MB) or
-    the string "unlimited"
-    EOT
-    newvalues(/\d{1,}/, "unlimited")
-    munge do |value|
-      value = -1 if value == "unlimited"
-      value
-    end
-  end
-
 end
